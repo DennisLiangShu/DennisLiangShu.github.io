45d156a2c68f268009a0ca13b78fb9358e5d64af
Dennis Liang <dennis@Denniss-MacBook-Pro.local>
2023-03-14 10:31:59 -0400

    phone width changes added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,17 +8,17 @@ function Footer() {
                 <h1>CONTACT</h1>
                 <div class="footer-center">
 
-                    <div>
+                    <div class="mbl-icon">
                         <a href="https://www.linkedin.com/in/dennis-liang-shu/"><i class="fa fa-linkedin"></i></a>
                         <a href="https://www.linkedin.com/in/dennis-liang-shu/"><p>LinkedIn</p></a>
                     </div>
 
-                    <div>
-                        <i class="fa fa-phone"></i>
-                        <p>+1 (215) 858 4783</p>
+                    <div class="mbl-icon">
+                        <a href="tel:+1215-858-4783"><i class="fa fa-phone"></i></a>
+                        <a href="tel:+1215-858-4783"><p>+1 (215) 858 4783</p></a>
                     </div>
 
-                    <div>
+                    <div class="mbl-icon">
                         <a href="mailto:d.l.s6465@gmail.com"><i class="fa fa-envelope"></i></a>
                         <a href="mailto:d.l.s6465@gmail.com"><p>d.l.s6465@gmail.com</p></a>
                     </div>
