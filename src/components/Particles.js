e5e8f5ff82beec6cff73db061b344c2ae11a9c88
Dennis Liang <dennis@Denniss-MacBook-Pro.local>
2023-06-28 22:00:24 -0400

    updated particle effect

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -1,108 +1,76 @@
+import React, { useCallback, useMemo } from "react";
 import Particles from "react-tsparticles";
-import { loadSlim } from "tsparticles-slim"; // loads tsparticles-slim
-//import { loadFull } from "tsparticles"; // loads tsparticles
-import { useCallback, useMemo } from "react";
+import { loadSlim } from "tsparticles-slim";
 
-// tsParticles Repository: https://github.com/matteobruni/tsparticles
-// tsParticles Website: https://particles.js.org/
-const ParticlesComponent = (props) => {
-  // using useMemo is not mandatory, but it's recommended since this value can be memoized if static
+const ParticlesComponent = () => {
   const options = useMemo(() => {
-    // using an empty options object will load the default options, which are static particles with no background and 3px radius, opacity 100%, white color
-    // all options can be found here: https://particles.js.org/docs/interfaces/Options_Interfaces_IOptions.IOptions.html
     return {
       background: {
-        color: "#000", // this sets a background color for the canvas
+        color: "#000000", // Dark background color
       },
       fullScreen: {
-        enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
-        zIndex: -1, // this is the z-index value used when the fullScreen is enabled, it's 0 by default
+        enable: true,
+        zIndex: -1,
       },
-      
-      fps_limit: 60,
-      interactivity: {
-        events: {
-          onclick: { enable: true, mode: "push"},
-          onhover: {
-            enable: true,
-            mode: "attract",
-            parallax: { enable: false, force: 60, smooth: 10 },
-          },
-          resize: true
-        },
-        modes: {
-          push: { quantity: 4 },
-          attract: { distance: 200, duration: 0.4, factor: 5 }
-        }
-      },
+      fpsLimit: 60,
       particles: {
-        color: { value: "#00C1D3" },
-        line_linked: {
-          color: "#00C1D3",
-          distance: 150,
-          enable: true,
-          opacity: 0.7,
-          width: 2
+        color: {
+          value: ["#FFFFFF", "#FF6B6B", "#F9A825", "#00C1D3"], // Add the desired colors to the array
         },
-        move: {
-          attract: { enable: false, rotateX: 600, rotateY: 1200 },
-          bounce: false,
-          direction: "none",
-          enable: true,
-          out_mode: "out",
-          random: false,
-          speed: 2,
-          straight: false
+        number: {
+          value: 400, // Adjust the number of particles 
         },
-        number: { density: { enable: true, value_area: 800 }, value: 50 },
         opacity: {
-          anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
-          random: false,
-          value: 0.9
-        },
-        shape: {
-          character: {
-            fill: false,
-            font: "Verdana",
-            style: "",
-            value: "*",
-            weight: "400"
+          value: 0.8, // Adjust the opacity of particles
+          random: true,
+          anim: {
+            enable: true,
+            speed: 1,
+            opacity_min: 0,
+            sync: false,
           },
-          image: {
-            height: 100,
-            replace_color: true,
-            src: "images/github.svg",
-            width: 100
-          },
-          polygon: { nb_sides: 5 },
-          stroke: { color: "#000000", width: 0 },
-          type: "circle"
         },
         size: {
-          anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+          value: 2, // Adjust the size of particles
           random: true,
-          value: 5
-        }
+          anim: {
+            enable: true,
+            speed: 1,
+            size_min: 0,
+            sync: false,
+          },
+        },
+        move: {
+          enable: true,
+          speed: 0.5, // Adjust the speed of particle movement as desired
+          direction: "top",
+          random: true,
+          straight: false,
+          out_mode: "out",
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
       },
-      polygon: {
-        draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
-        move: { radius: 10 },
-        scale: 1,
-        type: "none",
-        url: ""
+      interactivity: {
+        detectsOn: "canvas",
+        events: {
+          onhover: {
+            enable: false, // Disable hover interaction
+          },
+        },
       },
-      
+      retina_detect: true,
     };
   }, []);
 
-  // useCallback is not mandatory, but it's recommended since this callback can be memoized if static
   const particlesInit = useCallback((engine) => {
     loadSlim(engine);
-    // loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
   }, []);
 
-  // setting an id can be useful for identifying the right particles component, this is useful for multiple instances or reusable components
-  return <Particles id={props.id} init={particlesInit} options={options} />;
+  return <Particles init={particlesInit} options={options} />;
 };
 
 export default ParticlesComponent;
